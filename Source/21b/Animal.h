8d11fb745eaf3f28c2c1dda73594e915a6f8e447
Darko Draskovic <darko.draskovic@gmail.com>
2021-02-07 19:32:01 +0100

    Add Animal inheritance example
    Signed-off-by: Darko Draskovic <darko.draskovic@gmail.com>

---
+++ Source/21b/Animal.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class Animal
+{
+public:
+    Animal(const std::string& name) : name_(name) {
+        std::cout << "Animal Constructor!" << "\n";
+    }
+    ~Animal() {
+        std::cout << "Animal Destructor!" << "\n";
+    }
+private:
+    std::string name_;
+};
+
+class Dog : private Animal
+{
+public:
+    Dog(const std::string& name) : Animal(name) {
+        std::cout << "Dog Constructor!" << "\n";
+    }
+    ~Dog() {
+        std::cout << "Dog Destructor!" << "\n";
+    }
+};
---
+++ Source/21b/Main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+#include "Animal.h"
+
+int main(int argc, char *argv[]){
+    {
+        Animal a("fish");
+    }
+
+    std::cout << "" << "\n";
+
+    {
+        Dog d("Alfredo");
+    }
+    
+    return 0;
+}
